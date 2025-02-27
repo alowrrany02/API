2037eb8e43ae8fbf205d859f0fa7f4b6de24debe
alowrrany02 <130370163+alowrrany02@users.noreply.github.com>
2025-02-27 18:20:15 -0300

    Up

--- sever.js
+++ sever.js
@@ -1,16 +1,32 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.get('/usuarios', (req, res) =>{
-    
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+   users.push(req.body)
+
+    res.status(201).json(req.body)
 })
 
 
+app.get('/usuarios', (req, res) =>{
+    res.status(200).json(users)
+})
+
+app.listen(3000)
 
 
 
 
 /* 
 1- tipo de rota
-2- Endereço */
+2- Endereço
+
+lowrrany
+]xL_j6BzR%*R&xV
+
+*/
